66486f919876ad8669d5e7100a59b4e7c5820c47
Daniel <daniel@mdz.ro>
2018-07-15 20:55:08 +0100

    php and html

--- php-and-html/index.php
+++ php-and-html/index.php
@@ -1,10 +1,42 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
 
-$greeting = 'Hello';
+<style>
 
-$name = 'Jane Doe';
+	header {
+		background: #e3e3e3;
+		padding: 2em;
+		text-align: center;
+	}
 
+</style>
 
-echo "{$greeting}, {$name}";
+<body>
 
-?>
+	
+	<header>
+		
+		<h1>
+
+			<?php
+
+			 	$greeting = 'Hello World';
+
+			 	$name = htmlspecialchars($_GET['name']);
+
+			 	echo "Hello {$name}";
+
+			?>
+				
+		</h1>
+
+	</header>
+	
+
+</body>
+
+</html>
