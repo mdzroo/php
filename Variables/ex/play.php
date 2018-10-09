8562062ddb6577eefb62642d208e15db5fae7348
daniel <contact@mdz.ro>
2018-10-09 21:52:57 +0100

    play

---
+++ Variables/ex/play.php
@@ -0,0 +1,19 @@
+<?php
+
+$greeting = "hello boss";
+
+echo $greeting;
+
+echo '<br>';
+
+$myName = 'Daniel';
+
+echo 'Hello, $myName';
+
+echo '<br>';
+
+echo "Hello, $myName" . ' this is the correct way with double quotes';
+
+echo '<br>';
+
+echo "Hello, {$myName}";
